e1459f03ec5341b317238af0e46f600a241b2800
wrr606 <wrr606@gmai.com>
2023-05-14 00:10:53 +0800

    re

--- homework/mid/main.js
+++ homework/mid/main.js
@@ -1,16 +1,28 @@
+var range_max,range_min;
+
 function submit_function(){
     let number=parseInt(document.getElementById("input_number").value);
     document.getElementById("counter").innerHTML++;
     answer=parseInt(answer);
     if(answer>number){
+        document.getElementById("ans").style.display="block";
         document.getElementById("ans").innerHTML="數字太小";
+        if(range_min<number)
+            range_min=number;
+        document.getElementById("range").innerHTML=`${range_min}　　~　　${range_max}`;
     }
     else if(answer<number){
+        document.getElementById("ans").style.display="block";
         document.getElementById("ans").innerHTML="數字太大";
+        if(range_max>number)
+            range_max=number;
+        document.getElementById("range").innerHTML=`${range_min}　　~　　${range_max}`;
     }
     else{
+        document.getElementById("ans").style.display="block";
         document.getElementById("ans").innerHTML="答對：)";
-        bsa();
+        document.getElementById("range").innerHTML=`正確答案是　${answer}`;
+        document.getElementById("extension").style.display="block";
     }
     //document.getElementById("ans").innerHTML=number;
 }
@@ -18,6 +30,7 @@ function submit_function(){
 function bsa(){
     document.getElementById("introduction").style.display="block";
     document.getElementById("bsa").style.display="block";
+    document.getElementById("content").style.display="block";
 }
 
 function begin_game(){
@@ -29,16 +42,27 @@ function begin_game(){
     if(max==""){
         max=100;
     }
-    answer=Math.random()*(max-min)-min;
-    while(answer>max || answer<min){
+    if(max<min){
+        [max,min]=[min,max];
+    }
+    min=parseInt(min),max=parseInt(max);
+    document.getElementById("input_min").value=min;
+    document.getElementById("input_max").value=max;
+    if(min==max)
+        answer=min;
+    else{
         answer=Math.random()*(max-min)-min;
+        while(answer>max || answer<min){
+            answer=Math.random()*(max-min)-min;
+        }
     }
     document.getElementById("try").style.display="block";
     //document.getElementById("number").style.opacity = 1;
     document.getElementById("input_number").style.display="block";
     document.getElementById("submit").style.display="block";
     document.getElementById("counter").innerHTML=0;
-    document.getElementById("ans").innerHTML="";
+    document.getElementById("range").innerHTML=`${min}　　~　　${max}`;
+    range_max=max,range_min=min;
     //document.getElementById("ans").innerHTML=min;
 }
 
@@ -46,3 +70,4 @@ var answer;
 
 document.getElementById("submit").onclick=function(){submit_function()};
 document.getElementById("begin").onclick=function(){begin_game()};
+document.getElementById("extension").onclick=function(){bsa()};
